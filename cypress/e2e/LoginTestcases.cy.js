c43f3b470884b699b5b71cb9ce5cfd2b4c1523db
tahirahmadqa <95229345+tahirahmadqa@users.noreply.github.com>
2023-03-07 19:16:15 +0500

    Updated test cases
    Added test cases for registration
    Added Task 1 and Task2

--- cypress/e2e/LoginTestcases.cy.js
+++ cypress/e2e/LoginTestcases.cy.js
@@ -6,33 +6,38 @@ describe('Login functionality', () => {
     cy.visit('/')
   })
 
-  it('Enter Email Address with valid credentials',() => {
-      MainLoginPage.LoginUsername().type('email.tahirahmed@gmail.com');
-      MainLoginPage.LoginPassword().type('1234')
-      MainLoginPage.LoginButton().click()
+  it('Enter Email Address with valid credentials', () => {
+    MainLoginPage.LoginUsername().type('email.tahirahmed@gmail.com');
+    MainLoginPage.LoginPassword().type('1234')
+    MainLoginPage.LoginButton().click()
   })
 
-  it('Enter Email Address which is not register with web',() => {
-      MainLoginPage.LoginUsername().click().type('123@gmail.com');
-      MainLoginPage.LoginPassword().click().type('1234')
-      MainLoginPage.LoginButton().click()
+  it('Enter Email Address which is not register with web', () => {
+    MainLoginPage.LoginUsername().click().type('123@gmail.com');
+    MainLoginPage.LoginPassword().click().type('1234')
+    MainLoginPage.LoginButton().click()
   })
 
-  it('Enter Email Address with valid credentials',() => {
-      MainLoginPage.LoginUsername().click().type('email.tahirahmed@gmail.com');
-      MainLoginPage.LoginPassword().click().type('1234')
-      MainLoginPage.LoginButton().click()
+  it('Enter Email Address with valid credentials', () => {
+    MainLoginPage.LoginUsername().click().type('email.tahirahmed@gmail.com');
+    MainLoginPage.LoginPassword().click().type('1234')
+    MainLoginPage.LoginButton().click()
   })
 
-  it('Enter empty password',() => {
-      MainLoginPage.LoginUsername().click().type('email.tahirahmed@gmail.com');
-      MainLoginPage.LoginPassword().click()
-      MainLoginPage.LoginButton().click()
+  it('Enter empty password', () => {
+    MainLoginPage.LoginUsername().click().type('email.tahirahmed@gmail.com');
+    MainLoginPage.LoginPassword().click()
+    MainLoginPage.LoginButton().click()
+  })
+  it('Enter empty Email', () => {
+    MainLoginPage.LoginUsername().click();
+    MainLoginPage.LoginPassword().click().type('1234')
+    MainLoginPage.LoginButton().click()
+  })
+  it('Enter empty credentials', () => {
+    MainLoginPage.LoginUsername().click()
+    MainLoginPage.LoginPassword().click()
+    MainLoginPage.LoginButton().click()
   })
 
-  it('Enter empty credentials',() => {
-      MainLoginPage.LoginUsername().click()
-      MainLoginPage.LoginPassword().click()
-      MainLoginPage.LoginButton().click()
-  })
 })
--- cypress/e2e/RegistrationTestcases.cy.js
+++ cypress/e2e/RegistrationTestcases.cy.js
@@ -5,20 +5,41 @@ describe('Registration functionality', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it.only('Register Email Address with valid credentials',() => {
-      MainLoginPage.NewUserButton().click();
-      MainLoginPage.RegisterEmail().click().type(MainLoginPage.generateRandomEmail());
-      var pass =MainLoginPage.generateRandomPassword(10)
-      MainLoginPage.RegisterPassword().click().type(pass);
-      MainLoginPage.RegisterConfirmPassword().type(pass);
-      MainLoginPage.Register().click();
+
+  it('Register Email Address with valid credentials', () => {
+    MainLoginPage.NewUserButton().click();
+    MainLoginPage.RegisterEmail().click().type(MainLoginPage.generateRandomEmail());
+    var pass = MainLoginPage.generateRandomPassword(10)
+    MainLoginPage.RegisterPassword().click().type(pass);
+    MainLoginPage.RegisterConfirmPassword().type(pass);
+    MainLoginPage.Register().click();
   })
-  it('Register user with empty email and password', () =>{
-      MainLoginPage.NewUserButton().click();
-      MainLoginPage.RegisterEmail().click();
-      MainLoginPage.RegisterPassword().click();
-      MainLoginPage.RegisterConfirmPassword().click();
-      MainLoginPage.Register().click();
-      MainLoginPage.emptyError().should('be.visible');
+
+  it('Register user with empty email and password', () => {
+    MainLoginPage.NewUserButton().click();
+    MainLoginPage.RegisterEmail().click();
+    MainLoginPage.RegisterPassword().click();
+    MainLoginPage.RegisterConfirmPassword().click();
+    MainLoginPage.Register().click();
+    MainLoginPage.emptyError().should('be.visible');
+  })
+
+  it('Enters a Different password in the " confirm password " box', () => {
+    MainLoginPage.NewUserButton().click();
+    MainLoginPage.RegisterEmail().click().type(MainLoginPage.generateRandomEmail());
+    var pass = MainLoginPage.generateRandomPassword(10)
+    var value = MainLoginPage.generateRandomPassword(4)
+    MainLoginPage.RegisterPassword().click().type(pass);
+    MainLoginPage.RegisterConfirmPassword().type(value);
+    MainLoginPage.Register().click();
+  })
+
+  it('Enter an email that is already registered with the website', () => {
+    MainLoginPage.NewUserButton().click();
+    MainLoginPage.RegisterEmail().click().type('email.tahirahmad2@gmail.com');
+    var pass = MainLoginPage.generateRandomPassword(10)
+    MainLoginPage.RegisterPassword().click().type(pass);
+    MainLoginPage.RegisterConfirmPassword().type(pass);
+    MainLoginPage.Register().click();
   })
 })
